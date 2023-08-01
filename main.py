10f107bf3090744138532ea46eb2f28e6cd6614e
brunodos-santos <bruno6.santos@usp.br>
2023-08-01 14:55:06 -0300

    Melhorias no botão de salvar e criação do botão de geração de senha aleatória

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from tkinter import *
 from tkinter import messagebox
-import random
+from random import randint, choice, shuffle
+import pyperclip
 # ---------------------------- CONSTANTS ------------------------------- #
 RED = "#e7305b"
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-# string_list = ['a','b', 'c', 'd',]
-def password_generator():
-    pass
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+
+def password_generator():
+    password_entry.delete(0, END)
+
+    nr_letters = randint(8, 10)
+    nr_numbers = randint(2, 4)
+    nr_symbols = randint(2, 4)
+
+    password_list = [choice(letters) for _ in range(nr_letters)]
+    password_list += [choice(numbers) for _ in range(nr_numbers)]
+    password_list += [choice(symbols) for _ in range(nr_symbols)]
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.get()
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -64,7 +83,7 @@ password_entry = Entry(width=35)
 password_entry.grid(column=1, row=3)
 
 # Buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=password_generator)
 generate_password_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=45, command=save)
